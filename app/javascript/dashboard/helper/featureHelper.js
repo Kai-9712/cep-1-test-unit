f66380a4e1ad991675e7156b9476817d940e4e18
Pranav <pranav@chatwoot.com>
2024-08-06 20:58:04 -0700

    feat: Update the design of the audit logs page (#9901)
    This is continuation of the design update, updates the design for audit logs listing page.
    
    ---------
    
    Co-authored-by: Sivin Varghese <64252451+iamsivin@users.noreply.github.com>

--- app/javascript/dashboard/helper/featureHelper.js
+++ app/javascript/dashboard/helper/featureHelper.js
@@ -1,9 +1,13 @@
 const FEATURE_HELP_URLS = {
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> aea68f1ec (feat: Update the design of the audit logs page (#9901))
   agent_bots: 'https://chwt.app/hc/agent-bots',
   audit_logs: 'https://chwt.app/hc/audit-logs',
   campaigns: 'https://chwt.app/hc/campaigns',
   canned_responses: 'https://chwt.app/hc/canned',
+<<<<<<< HEAD
   channel_email: 'https://chwt.app/hc/email',
   channel_facebook: 'https://chwt.app/hc/fb',
   custom_attributes: 'https://chwt.app/hc/custom-attributes',
@@ -16,21 +20,23 @@ const FEATURE_HELP_URLS = {
   sla: 'https://chwt.app/hc/sla',
   team_management: 'https://chwt.app/hc/teams',
 =======
+=======
+>>>>>>> aea68f1ec (feat: Update the design of the audit logs page (#9901))
   channel_email: 'https://chwt.app/hc/email',
   channel_facebook: 'https://chwt.app/hc/fb',
-  help_center: 'https://chwt.app/hc/help-center',
-  agent_bots: 'https://chwt.app/hc/agent-bots',
-  team_management: 'https://chwt.app/hc/teams',
-  labels: 'https://chwt.app/hc/labels',
   custom_attributes: 'https://chwt.app/hc/custom-attributes',
-  canned_responses: 'https://chwt.app/hc/canned',
-  integrations: 'https://chwt.app/hc/integrations',
-  campaigns: 'https://chwt.app/hc/campaigns',
-  reports: 'https://chwt.app/hc/reports',
-  message_reply_to: 'https://chwt.app/hc/reply-to',
-  sla: 'https://chwt.app/hc/sla',
   dashboard_apps: 'https://chwt.app/hc/dashboard-apps',
+<<<<<<< HEAD
 >>>>>>> 6694db093 (feat: Update the design for dashboard_apps (#9840))
+=======
+  help_center: 'https://chwt.app/hc/help-center',
+  integrations: 'https://chwt.app/hc/integrations',
+  labels: 'https://chwt.app/hc/labels',
+  message_reply_to: 'https://chwt.app/hc/reply-to',
+  reports: 'https://chwt.app/hc/reports',
+  sla: 'https://chwt.app/hc/sla',
+  team_management: 'https://chwt.app/hc/teams',
+>>>>>>> aea68f1ec (feat: Update the design of the audit logs page (#9901))
 };
 
 export function getHelpUrlForFeature(featureName) {
