262cc9b3ab6eb368889ca60794a5a9b7d22fbe4a
Sivin Varghese <64252451+iamsivin@users.noreply.github.com>
2024-08-09 18:40:06 +0530

    feat: Replace the use of `macroMixin` with a composable (#9912)
    This PR will replace usage of `macroMixin` with the `useMacros`
    composable. And updated components from option API to composition API.
    
    **Files updated**
    1. dashboard/routes/dashboard/settings/macros/MacroNode.vue
    2. dashboard/routes/dashboard/settings/macros/MacroEditor.vue
    
    Fixes
    https://linear.app/chatwoot/issue/CW-3449/rewrite-macrosmixin-mixin-to-a-composable
    
    - [x] New feature (non-breaking change which adds functionality)
    
    **Test cases**
    1. Check whether we can create a new macro.
    2. Check whether validations and error animation are working or not.
    3. Ability to drag the macro files
    4. Check whether the edit pages and functionality is working or not.
    
    - [x] My code follows the style guidelines of this project
    - [x] I have performed a self-review of my code
    - [x] I have commented on my code, particularly in hard-to-understand
    areas
    - [ ] I have made corresponding changes to the documentation
    - [x] My changes generate no new warnings
    - [x] I have added tests that prove my fix is effective or that my
    feature works
    - [x] New and existing unit tests pass locally with my changes
    - [ ] Any dependent changes have been merged and published in downstream
    modules

--- app/javascript/dashboard/routes/dashboard/settings/macros/MacroNode.vue
+++ app/javascript/dashboard/routes/dashboard/settings/macros/MacroNode.vue
@@ -1,8 +1,12 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> 3558878ae (feat: Replace the use of `macroMixin` with a composable (#9912))
 <script setup>
 import { computed, inject } from 'vue';
 import { useMacros } from 'dashboard/composables/useMacros';
 import { useI18n } from 'dashboard/composables/useI18n';
+<<<<<<< HEAD
 import ActionInput from 'dashboard/components/widgets/AutomationActionInput.vue';
 
 const props = defineProps({
@@ -118,85 +122,81 @@ const dropdownValues = () => {
 >>>>>>> b4b308336 (feat: Eslint rules (#9839))
 <script>
 import { inject } from 'vue';
+=======
+>>>>>>> 3558878ae (feat: Replace the use of `macroMixin` with a composable (#9912))
 import ActionInput from 'dashboard/components/widgets/AutomationActionInput.vue';
-import macrosMixin from 'dashboard/mixins/macrosMixin';
 
-export default {
-  components: {
-    ActionInput,
+const props = defineProps({
+  singleNode: {
+    type: Boolean,
+    default: false,
   },
-  mixins: [macrosMixin],
-  props: {
-    singleNode: {
-      type: Boolean,
-      default: false,
-    },
-    value: {
-      type: Object,
-      default: () => ({}),
-    },
-    errorKey: {
-      type: String,
-      default: '',
-    },
-    fileName: {
-      type: String,
-      default: '',
-    },
+  value: {
+    type: Object,
+    default: () => ({}),
   },
-  setup() {
-    const macroActionTypes = inject('macroActionTypes');
-    return { macroActionTypes };
+  errorKey: {
+    type: String,
+    default: '',
   },
-  computed: {
-    actionData: {
-      get() {
-        return this.value;
-      },
-      set(value) {
-        this.$emit('input', value);
-      },
-    },
-    errorMessage() {
-      if (!this.errorKey) return '';
+  fileName: {
+    type: String,
+    default: '',
+  },
+});
 
-      return this.$t(`MACROS.ERRORS.${this.errorKey}`);
-    },
-    showActionInput() {
-      if (
-        this.actionData.action_name === 'send_email_to_team' ||
-        this.actionData.action_name === 'send_message'
-      )
-        return false;
-      const type = this.macroActionTypes.find(
-        action => action.key === this.actionData.action_name
-      ).inputType;
-      return !!type;
-    },
-  },
-  methods: {
-    dropdownValues() {
-      return this.getDropdownValues(this.value.action_name, this.$store);
-    },
-  },
+const emit = defineEmits(['input', 'resetAction', 'deleteNode']);
+
+const { t } = useI18n();
+
+const macroActionTypes = inject('macroActionTypes');
+
+const { getMacroDropdownValues } = useMacros();
+
+const actionData = computed({
+  get: () => props.value,
+  set: value => emit('input', value),
+});
+
+const errorMessage = computed(() => {
+  if (!props.errorKey) return '';
+  return t(`MACROS.ERRORS.${props.errorKey}`);
+});
+
+const showActionInput = computed(() => {
+  if (
+    actionData.value.action_name === 'send_email_to_team' ||
+    actionData.value.action_name === 'send_message'
+  )
+    return false;
+  const type = macroActionTypes.find(
+    action => action.key === actionData.value.action_name
+  ).inputType;
+  return !!type;
+});
+
+const dropdownValues = () => {
+  return getMacroDropdownValues(props.value.action_name);
 };
 </script>
 
 <template>
-  <div class="macro__node-action-container">
+  <div class="relative flex items-center w-full min-w-0 basis-full">
     <woot-button
       v-if="!singleNode"
       size="small"
       variant="clear"
       color-scheme="secondary"
       icon="navigation"
-      class="macros__node-drag-handle"
+      class="absolute cursor-move -left-8 macros__node-drag-handle"
     />
     <div
-      class="macro__node-action-item"
-      :class="{
-        'has-error': errorKey,
-      }"
+      class="flex-grow p-2 mr-2 rounded-md shadow-sm"
+      :class="
+        errorKey
+          ? 'bg-red-50 animate-shake dark:bg-red-800'
+          : 'bg-white dark:bg-slate-700'
+      "
     >
       <ActionInput
         v-model="actionData"
@@ -221,6 +221,7 @@ export default {
     />
   </div>
 </template>
+<<<<<<< HEAD
 
 <style scoped lang="scss">
 .macros__node-drag-handle {
@@ -258,3 +259,5 @@ export default {
 }
 </style>
 >>>>>>> ce8e1ec93 (chore: Migrate all instances of old vuelidate to new v2 syntax [CW-3274] (#9623))
+=======
+>>>>>>> 3558878ae (feat: Replace the use of `macroMixin` with a composable (#9912))
