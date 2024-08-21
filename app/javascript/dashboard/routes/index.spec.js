221565ad07136b031879489b62ad69ea608486c2
Pranav <pranav@chatwoot.com>
2024-08-21 11:36:26 +0530

    fix: Remove user.permissions, resolve it from accounts (#9990)
    Remove the `user.permissions` field and resolve the permissions directly
    from the accounts array in the user. This change ensures that the cache
    or previous values from the last active account don't affect the
    permissions.
    
    In this PR:
    - Remove user.permissions usage, replace it with getUserPermissions
    method.
    - Remove json.permissions from user.json.jbuilder

--- app/javascript/dashboard/routes/index.spec.js
+++ app/javascript/dashboard/routes/index.spec.js
@@ -38,7 +38,18 @@ describe('#validateAuthenticateRoutePermission', () => {
             getCurrentUser: {
               account_id: 1,
               id: 1,
+<<<<<<< HEAD
               accounts: [{ id: 1, role: 'agent', status: 'active' }],
+=======
+              accounts: [
+                {
+                  permissions: ['agent'],
+                  id: 1,
+                  role: 'agent',
+                  status: 'active',
+                },
+              ],
+>>>>>>> 77b718c22 (fix: Remove user.permissions, resolve it from accounts (#9990))
             },
           };
           validateAuthenticateRoutePermission(to, next, { getters });
@@ -55,7 +66,18 @@ describe('#validateAuthenticateRoutePermission', () => {
             getCurrentUser: {
               account_id: 1,
               id: 1,
+<<<<<<< HEAD
               accounts: [{ id: 1, role: 'agent', status: 'active' }],
+=======
+              accounts: [
+                {
+                  id: 1,
+                  role: 'administrator',
+                  permissions: ['administrator'],
+                  status: 'active',
+                },
+              ],
+>>>>>>> 77b718c22 (fix: Remove user.permissions, resolve it from accounts (#9990))
             },
           };
           validateAuthenticateRoutePermission(to, next, { getters });
